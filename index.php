121aa27e44d27eeedbd88f4a7dcc40f4bd029f59
Christian-Carminati <03chriscar@gmail.com>
2022-02-01 07:26:48 +0000

    first-commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $file_data = json_decode(file_get_contents("dati.json"));
+    foreach ($file_data as $key => $value) {
+        $d = $value -> dato;
+        echo "[$key] $d<br>";
+    }
+    $newValore = new stdClass();
+    $newValore -> dato = ($d + 1);
+    $file_data[] = $newValore;
+
+    file_put_contents("dati.json",json_encode($file_data));
+?>
